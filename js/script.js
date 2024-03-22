558a6b82ceb690fdd6639bf88dd5d61b5da18ec6
aniellonigrocogliano <aniello.nigro.cogliano@gmail.com>
2024-03-22 15:01:33 +0100

    add: console log per le variabili UerName Usersurname e UserColor

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 let userName = prompt("Ciao utente! Qual'è il tuo nome?"); // string
 let userSurname = prompt("Ciao " + userName + " Qual'è il tuo cognome?"); // string
 let userColor = prompt("Scusami " + userName + " ma posso sapera anche il tuo colore preferito?"); // string
+console.log(userName, typeof userName, );
+console.log(userSurname, typeof userSurname,);
+console.log( userColor, typeof userColor);
