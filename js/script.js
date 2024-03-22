2ee4783fe39abe2cbce11833080859853991ca9b
aniellonigrocogliano <aniello.nigro.cogliano@gmail.com>
2024-03-22 14:53:47 +0100

    add: variabili userName UseSurname userColor

---
+++ js/script.js
@@ -0,0 +1,3 @@
+let userName = prompt("Ciao utente! Qual'è il tuo nome?"); // string
+let userSurname = prompt("Ciao " + userName + " Qual'è il tuo cognome?"); // string
+let userColor = prompt("Scusami " + userName + " ma posso sapera anche il tuo colore preferito?"); // string
