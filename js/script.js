39e30f0e6ec60cf9fd3b904e055c6de7fa6c920b
aniellonigrocogliano <aniello.nigro.cogliano@gmail.com>
2024-03-22 15:29:18 +0100

    add: risultato con numero random

--- js/script.js
+++ js/script.js
@@ -9,10 +9,19 @@ console.log(userName, typeof userName, );
 console.log(userSurname, typeof userSurname,);
 console.log( userColor, typeof userColor);
 //creazione della variabile costante 21
-const number21 = 21; //number
+let number21 = 21; //number
+number21 = number21.toString(); //string
 console.log( number21, typeof number21);
 //concatenazione dei dati
 let result = userName+userSurname+userColor+number21; //string
 console.log( result, typeof result);
 //stampa nel codice html
 document.getElementById("answer").innerHTML = result;
+//Generare numero random
+let numberRandom =  Math.floor(Math.random() * 100); //number
+numberRandom = numberRandom.toString(); //string
+console.log( numberRandom, typeof numberRandom);
+//concatenazione dei dati con numero random
+let result2 = userName+userSurname+userColor+numberRandom; //string
+//stampa nel codice html
+document.getElementById("answer2").innerHTML = result2;
